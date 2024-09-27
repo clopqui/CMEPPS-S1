448f2f932e9c359f17ba2a01d9d8ab299517c824
UnclearFrog30 <32170953+UnclearFrog30@users.noreply.github.com>
2024-09-27 13:16:43 +0200

    Creacion del branch, Nodo 2

--- src/sesion1.java
+++ src/sesion1.java
@@ -4,6 +4,8 @@ public class sesion1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Este es el Nodo 1");
+		System.out.println("Se crea el branch, Nodo 2");
+		
 	}
 
 }
