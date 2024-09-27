ef779c590d8e8f88e87739fa25f72d025de08520
UnclearFrog30 <32170953+UnclearFrog30@users.noreply.github.com>
2024-09-27 13:23:51 +0200

    Se añade el merge, Nodo 4.

--- src/sesion1.java
+++ src/sesion1.java
@@ -6,6 +6,7 @@ public class sesion1 {
 		System.out.println("Este es el Nodo 1");
 		System.out.println("Se crea el branch, Nodo 2");
 		System.out.println("Este es el Nodo 3");
+		System.out.println("Se hace un merge con el master, Nodo 4");
 	}
 
 }
